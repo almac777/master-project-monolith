0bc312ea75876aa433bf072fa32a8c5c2532648a
Alex Macovei <Alex.Macovei@twinformatics.at>
2019-11-19 20:07:21 +0100

    smoketest has been extended

--- src/test/java/at/ac/fhcampus/master/monolith/MonolithApplicationTests.java
+++ src/test/java/at/ac/fhcampus/master/monolith/MonolithApplicationTests.java
@@ -1,8 +1,12 @@
 package at.ac.fhcampus.master.monolith;
 
 import at.ac.fhcampus.master.monolith.articles.controllers.ArticleController;
+import at.ac.fhcampus.master.monolith.articles.repositories.ArticleRepository;
 import at.ac.fhcampus.master.monolith.auth.controllers.RegistrationController;
+import at.ac.fhcampus.master.monolith.auth.repositories.AuthorityRepository;
+import at.ac.fhcampus.master.monolith.auth.repositories.UserRepository;
 import at.ac.fhcampus.master.monolith.ratings.controllers.RatingsController;
+import at.ac.fhcampus.master.monolith.ratings.repositories.RatingRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,10 +20,21 @@ class MonolithApplicationTests {
     @Autowired private RatingsController ratingsController;
     @Autowired private RegistrationController registrationController;
 
+    @Autowired private ArticleRepository articleRepository;
+    @Autowired private RatingRepository ratingRepository;
+    @Autowired private UserRepository userRepository;
+    @Autowired private AuthorityRepository authorityRepository;
+
+
     @Test
     void contextLoads() {
         assertThat(articleController).isNotNull();
         assertThat(ratingsController).isNotNull();
         assertThat(registrationController).isNotNull();
+
+        assertThat(articleRepository).isNotNull();
+        assertThat(ratingRepository).isNotNull();
+        assertThat(userRepository).isNotNull();
+        assertThat(authorityRepository).isNotNull();
     }
 }
